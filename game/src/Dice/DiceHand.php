ad9efd41c8207847856e5325b5519c1951bb23a2
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-04-12 05:26:38 +0200

    Add DiceHand

---
+++ game/src/Dice/DiceHand.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pene14\Dice;
+
+class DiceHand
+// class DieHand
+{
+    private $die;
+
+    public function __construct($numberDie = 3) {
+        for ($i = 0; $i < $numberDie; $i++) {
+            $this->die[$i] = new GraphicalDice();
+        }
+    }
+
+    public function rollDie() {
+        foreach (array_values($this->die) as $dice) {
+            $dice->rollDice();
+        }
+    }
+
+    public function getLastRolls() {
+        $items = array();
+
+        foreach (array_values($this->die) as $dice) {
+            $items[] = $dice->getLastRoll();
+        }
+        return implode(',', $items) . ' = ' . array_sum($items);
+    }
+
+    public function graphicalDie() {
+        $items = array();
+
+        foreach (array_values($this->die) as $dice) {
+            $items[] = $dice->graphical();
+        }
+
+        return implode("", $items) ;
+    }
+}
