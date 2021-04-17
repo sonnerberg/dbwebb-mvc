b5577bfcb68d322584b7206d095c1785e8ce672b
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-04-17 21:31:23 +0200

    Update Game.php

--- game/src/Dice/Game.php
+++ game/src/Dice/Game.php
@@ -5,35 +5,29 @@ declare(strict_types=1);
 namespace Pene14\Dice;
 
 use function Mos\Functions\{
-    // destroySession,
-    redirectTo,
-    // renderView,
     renderTwigView,
-    sendResponse,
-    url
+    sendResponse
 };
 
 
 class Game
 {
-public function playGame() {
+    public function playGame() {
+        $dice = new GraphicalDice();
 
-    $dice = new GraphicalDice();
-
-    $dice->setSides(20);
-    $dice->rollDice();
-    // $dieHand = new DieHand(5);
-    $dieHand = new DiceHand(5);
-    $dieHand->rollDie();
-    $data = [
-        "header" => "Dice",
-        "message" => "Hey,play some die!",
-        "lastRoll" => $dice->getLastRoll(),
-        "lastRolls" => $dieHand->getLastRolls(),
-        "graphicalTest" => $dice->graphical(),
-        "graphical" => $dieHand->graphicalDie(),
-    ];
-    $body = renderTwigView("dice.html", $data);
-    sendResponse($body);
-}
+        $dice->setSides(20);
+        $dice->rollDice();
+        $dieHand = new DiceHand(5);
+        $dieHand->rollDie();
+        $data = [
+            "header" => "Dice",
+            "message" => "Hey,play some die!",
+            "lastRoll" => $dice->getLastRoll(),
+            "lastRolls" => $dieHand->getLastRolls(),
+            "graphicalTest" => $dice->graphical(),
+            "graphical" => $dieHand->graphicalDie(),
+        ];
+        $body = renderTwigView("dice.html", $data);
+        sendResponse($body);
+    }
 }
